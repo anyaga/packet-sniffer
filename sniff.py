a0b94534e7139efb9dc2a0e040793b55400a2b00
anyaga <nyaga.angel@outlook.com>
2025-09-06 00:51:35 -0500

    basic outline of packet snifer

--- sniff.py
+++ sniff.py
@@ -1,3 +1,19 @@
-import socket
-import struct
-import textwrap
+from scapy.all import sniff, IP, TCP, UDP, ICMP, wrpcap
+
+file = "found_packets.pcap"
+found = []
+
+def callback(packet):
+    return
+
+def main():
+    try: 
+        sniff(prn=callback,store=False)
+    except KeyboardInterrupt:
+        if found:
+            wrpcap(file,found)
+
+
+if __name__ == "__main__":
+    main()
+
